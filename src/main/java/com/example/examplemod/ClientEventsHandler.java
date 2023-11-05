394a3effebb24e6197edf9ddadd5c6db78ca94a5
winterSteve25 <w1nter.ish3re@gmail.com>
2023-11-05 10:38:51 -0500

    Example mod template for forge

---
+++ src/main/java/com/example/examplemod/ClientEventsHandler.java
@@ -0,0 +1,19 @@
+package com.example.examplemod;
+
+import com.github.wintersteve25.tau.renderer.ScreenUIRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
+
+@Mod.EventBusSubscriber(modid = ExampleMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ClientEventsHandler {
+    @SubscribeEvent
+    public static void onKeyPress(InputEvent.KeyInputEvent evt) {
+        if (evt.getKey() == GLFW.GLFW_KEY_COMMA) {
+            Minecraft.getInstance().setScreen(new ScreenUIRenderer(new ExampleUI()));
+        }
+    }
+}
